7061aef06aaaf1b4173020875fb8b08497f98806
Piyush Bheda <mr.bheda7706@gmail.com>
2026-06-11 18:58:08 +0530

    add login page

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './database/db.js';
+import cookieParser from 'cookie-parser';
+import authRoute from './routes/authRoute.js'
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT;
+
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+//routes
+app.use("/api/auth", authRoute);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
