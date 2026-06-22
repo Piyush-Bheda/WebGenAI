89f428fb9b0e7177f47a196e8ce9a924f45562b4
Piyush Bheda <mr.bheda7706@gmail.com>
2026-06-22 22:42:41 +0530

    add generate  page

--- frontend/vite-project/src/pages/WebsiteEditor.jsx
+++ frontend/vite-project/src/pages/WebsiteEditor.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import axios from 'axios'
+import { Code2, MessageSquare, Monitor, Rocket, Send, X } from 'lucide-react'
+import { AnimatePresence, motion } from 'motion/react'
+import React, { useEffect, useRef, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Editor from '@monaco-editor/react';
+
 
 function WebsiteEditor() {
+    const [website, setWebsite] = useState(null)
+    const [error, setError] = useState("")
+    const [code, setCode] = useState("")
+    const [messages, setMessages] = useState([])
+    const [prompt, setPrompt] = useState("")
+    const { id } = useParams()
+    const iframeRef = useRef(null)
+    const [updateLoading, setUpdateLoading] = useState(false)
+    const [thinkingIndex, setThinkingIndex] = useState(0)
+    const [showCode, setShowCode] = useState(false)
+    const [showFullPreview, setShowFullPreview] = useState(false)
+    const [showChat, setShowChat] = useState(false)
+    const thinkingSteps = [
+        "Understanding your request...",
+        "Planning layout changes...",
+        "Improving responsiveness...",
+        "Applying animations...",
+        "Finalizing Update..."
+    ]
+
+    
+        useEffect(() => {
+        const handleGetWebsite = async () => {
+            try {
+                const result = await axios.get(`${import.meta.env.VITE_SERVER_URL}/api/website/getbyid/${id}`, { withCredentials: true })
+                setWebsite(result.data)
+                setCode(result.data.latestCode)
+                setMessages(result.data.conversation)
+            } catch (error) {
+                setError(error.response.data.message)
+                console.log(error)
+            }
+        }
+        handleGetWebsite()
+    }, [id])
+
     return (
-        <div>WebsiteEditor</div>
+        <div className='h-screen w-screen flex bg-black text-white overflow-hidden'>
+
+        <aside className='hidden lg:flex w-95 flex-col border-r border-white/10 bg-black/80'>
+                <Header />
+                
+            </aside>
+
+        </div>
     )
+
+    function Header() {
+        return (
+            <div className='h-14 px-4 flex items-center justify-between border-b border-white/10'>
+                <span className='font-semibold truncate'>{website.title}</span>
+                <button onClick={()=>setShowChat(false)} className='lg:hidden'><X/></button>
+            </div>
+        )
+    }
 }
 
 export default WebsiteEditor
